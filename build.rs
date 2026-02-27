7e4f838fb959d943ee07650f602ec5b36b214921
Yvette Carlisle <y@acg.box>
2026-02-27 22:47:29 +0800

    {"schema":"maestro/commit/1","summary":"bootstrap voxit repository","authority":"manual","breaking":false}

---
+++ build.rs
@@ -0,0 +1,18 @@
+use std::error::Error;
+
+use vergen_gitcl::{CargoBuilder, Emitter, GitclBuilder};
+
+fn main() -> Result<(), Box<dyn Error>> {
+	let mut emitter = Emitter::default();
+
+	emitter.add_instructions(&CargoBuilder::default().target_triple(true).build()?)?;
+
+	// Disable the git version if installed from <https://crates.io>.
+	if emitter.add_instructions(&GitclBuilder::default().sha(true).build()?).is_err() {
+		println!("cargo:rustc-env=VERGEN_GIT_SHA=crates.io");
+	}
+
+	emitter.emit()?;
+
+	Ok(())
+}
